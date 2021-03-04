6533081bfdd3ef6802b7e23734f77922b4baa9ea
CODe1995 <code.57x53@gmail.com>
2021-03-04 11:13:29 +0900

    background 끊기는 이슈 수정

--- js/play.js
+++ js/play.js
@@ -2,33 +2,25 @@
 var imgNum = 1;
 var img = new Image()
 var canvas = document.getElementById('background');
-var ctx = canvas.getContext('2d');
+var context = canvas.getContext('2d');
+
 var root = "./res/img/";
 var maximgNum = 350;
-playSequence();
 
+var count = 0;
+function render(){
+    if(count>=maximgNum)return;
+    count+=1;
+    img.src = root+count+'.jpg';    
+    sleep(10);
+}
+function sleep(ms){
+    const wakeUpTime = Date.now() + ms;
+    while(Date.now() < wakeUpTime){}
+}
 img.src = root+"1.jpg";
-
-function playSequence() {
-    var timer = setInterval(function() {
-        console.log("Time Interval");
-        
-        if (imgNum >= maximgNum) {
-            clearInterval(timer);//한 번만 재생하고 멈춤
-        }
-        
-        player(imgNum);
-        imgNum++;
-    }, 10);
-}
-
-function player(num) {
-    console.log("image num: " + num);
-    img.src = root + num + ".jpg";
-}
-
 img.addEventListener('load', function(e) {
-    console.log("img load");    
-    ctx.clearRect(0,0,ctx.canvas.width,ctx.canvas.height);
-    ctx.drawImage(img,0,0,ctx.canvas.width,ctx.canvas.height);
+    // context.clearRect(0,0,canvas.width,canvas.height);
+    context.drawImage(img,0,0);
+    render();
 });
