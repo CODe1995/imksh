b6f05636d0c91dcbbb78eab893cb61045a8520c4
CODe1995 <code.57x53@gmail.com>
2021-03-05 01:09:02 +0900

    drawImage 속도 개선 완료
    프레임드랍 현상은 이미지를 불러오는데 문제가 있었고,
    모든 이미지를 먼저 불러온 후에 drawImage 과정을 실행한다.

--- js/play.js
+++ js/play.js
@@ -7,20 +7,27 @@ var context = canvas.getContext('2d');
 var root = "./res/img/";
 var maximgNum = 350;
 
+var imgs = []; //이미지 배열
+
+for(let i =1;i<=maximgNum;i++){
+    let tmpimg = new Image();
+    tmpimg.src=root+i+'.jpg';
+    imgs.push(tmpimg);
+    tmpimg.onload=function(){
+        if(i==maximgNum){
+            drawBackground();
+        }
+        // console.log("tmpimg loaded"+i);
+    }
+}
+
 var count = 0;
-function render(){
-    if(count>=maximgNum)return;
-    count+=1;
-    img.src = root+count+'.jpg';    
-    sleep(10);
+function drawBackground(){
+    if(count>=maximgNum){
+        window.cancelAnimationFrame(loop);
+        return;
+    }
+    context.drawImage(imgs[count],0,0);
+    count+=1; 
+    var loop = window.requestAnimationFrame(drawBackground);
 }
-function sleep(ms){
-    const wakeUpTime = Date.now() + ms;
-    while(Date.now() < wakeUpTime){}
-}
-img.src = root+"1.jpg";
-img.addEventListener('load', function(e) {
-    // context.clearRect(0,0,canvas.width,canvas.height);
-    context.drawImage(img,0,0);
-    render();
-});
